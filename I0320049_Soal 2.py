908e4be94a4f962adb5d4fa9cd171cb6184c97fb
I Gusti Ayu Ratu Putri Maharani <ayuratuputrimaha@gmail.com>
2021-04-16 12:15:17 +0800

    soal 2

---
+++ I0320049_Soal 2.py
@@ -0,0 +1,16 @@
+import math
+a = 89.9
+b = 70.5
+print("a = ", a)
+print("b = ", b)
+#mencari nilai max dan min
+print("nilai maximum = ", max(a,b))
+print("nilai minimum = ", min(a,b))
+
+#pembulatan bilangan riil ke bilangan bulat dibawahnya
+print("math.floor a = ", math.floor(a))
+print("math.floor b = ", math.floor(b))
+
+#pembulatan bilangan rril ke bilangan bulat diatasnya
+print("nilai a ceil = ", math.ceil(a))
+print("nilai b ceil = ", math.ceil(b))
