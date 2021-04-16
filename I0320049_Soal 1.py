3783241131101def295c079a403905294e845520
I Gusti Ayu Ratu Putri Maharani <ayuratuputrimaha@gmail.com>
2021-04-16 12:14:55 +0800

    soal 1

---
+++ I0320049_Soal 1.py
@@ -0,0 +1,10 @@
+str = "selamat pagi!"
+a = str.capitalize()
+print(a)
+b = str.count('a')
+c = str.count ('i')
+print("banyaknya huruf a= ", b)
+print("banyaknya huruf i= ", c)
+print(str.endswith("pagii"))
+print(str.endswith("pagi!"))
+
